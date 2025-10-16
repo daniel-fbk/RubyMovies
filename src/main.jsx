f777189915559356ef2d964ff118ca8a7603d055
Daniel Borsheim <daniel01.kodehode@gmail.com>
2025-10-16 22:21:22 +0200

    chore: fix basename for router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./lib/queryClient.js";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  basename: "RubyMovies",
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
